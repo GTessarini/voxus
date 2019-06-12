b8b0c52f38697c0bc7a9255fa616e41f4b256301
Gabriel Tessarini <gtessarini@uol.com.br>
2019-06-12 13:09:19 -0300

    Voxus - Ajustando estilos e validações de preenchimento de campos

--- script/main.js
+++ script/main.js
@@ -3,6 +3,7 @@ document.querySelector("#vx-button-experimentar").addEventListener("click", func
 }, false);
 
 function experimentar(){
+    console.log("experimentar");
     const buttonExperimentar = document.querySelector("#vx-button-experimentar");
     const contactMessage = document.querySelector("#vx-contact-message");
     const trialMessage = document.querySelector("#vx-trial-message"); 
@@ -24,7 +25,31 @@ function experimentar(){
 }
 
 function validateFields(){
-
+    let validation = {status: false};
+    const errorsMessages = {
+        "incomplete": "Este campo é obrigatório!",
+        "telefone": "Telefone inválido.",
+        "email": "E-mail inválido.",
+        "url": "Domínio inválido."
+    };
+    const fields = document.querySelectorAll(".vx-field input, .vx-field select");
+    fields.forEach(field => {
+        if(!field.value){
+            validation.errorMessage = errorsMessages["incomplete"];           
+        }else{
+            /* Implementar validação de cada tipo de campo */
+            const fieldNameValue= field.name;
+            const patternPhone = /^\d{10}$/;
+            if(
+                fieldNameValue = "telefone" && !field.value.match(patternPhone)
+            ){
+                validation.errorMessage = errorMessages[fieldNameValue] || errorMessages["incomplete"];
+            }else{
+                validation.status = true;
+            }           
+        }        
+    });
+    return validation;
 }
 
 /* Gabriel Tessarini */
